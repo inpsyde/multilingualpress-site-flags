a80c64c34e4f11a6668899abfe2088dd8335c623
Guido Scialfa <dev@guidoscialfa.com>
2019-01-25 14:44:06 +0100

    Prevent the plugin to thrown a fatal error if MultilingualPress isn't active
    Plus, the entire code of the main plugin file is under an Iife to avoid
    expose unnecessary functions.

--- multilingualpress-site-flags.php
+++ multilingualpress-site-flags.php
@@ -1,4 +1,5 @@
 <?php # -*- coding: utf-8 -*-
+// phpcs:disable
 /*
  * This file is part of the MultilingualPress package.
  *
@@ -22,10 +23,6 @@
  * Requires PHP: 7.0
  */
 
-// phpcs:disable PSR1.Files.SideEffects
-// phpcs:disable NeutronStandard.StrictTypes.RequireStrictTypes
-// phpcs:disable Inpsyde.CodeQuality.ReturnTypeDeclaration
-
 namespace Inpsyde\MultilingualPress\Flags;
 
 use Inpsyde\MultilingualPress\Flags\Flag;
@@ -33,13 +30,71 @@ use Inpsyde\MultilingualPress\Framework\Service\ServiceProvidersCollection;
 
 defined('ABSPATH') or die();
 
-if (version_compare(PHP_VERSION, '7', '<')) {
-    $hooks = [
-        'admin_notices',
-        'network_admin_notices',
-    ];
-    foreach ($hooks as $hook) {
-        add_action($hook, function () {
+(function () {
+    /**
+     * @param $function
+     */
+    function deactivateNotice($function)
+    {
+        if (!is_callable($function)) {
+            return;
+        }
+        $hooks = [
+            'admin_notices',
+            'network_admin_notices',
+        ];
+        foreach ($hooks as $hook) {
+            add_action($hook, function () use ($function) {
+                $function();
+
+                deactivate_plugins(plugin_basename(__FILE__));
+
+                // Suppress the "Plugin activated" notice.
+                unset($_GET['activate']); // phpcs:ignore
+            });
+        }
+    }
+
+    function requirements()
+    {
+        if (!function_exists('is_plugin_active')) {
+            /** @noinspection PhpIncludeInspection */
+            require_once untrailingslashit(ABSPATH) . '/wp-admin/includes/plugin.php';
+        }
+    }
+
+    function autoload()
+    {
+        static $done;
+        if (is_bool($done)) {
+            return $done;
+        }
+        if (class_exists(Flag\Flag::class)) {
+            $done = true;
+
+            return true;
+        }
+        if (is_readable(__DIR__ . '/autoload.php')) {
+            require_once __DIR__ . '/autoload.php';
+            $done = true;
+
+            return true;
+        }
+        if (is_readable(__DIR__ . '/vendor/autoload.php')) {
+            require_once __DIR__ . '/vendor/autoload.php';
+            $done = true;
+
+            return true;
+        }
+        $done = false;
+
+        return false;
+    }
+
+    requirements();
+
+    if (version_compare(PHP_VERSION, '7', '<')) {
+        deactivateNotice(function () {
             $message = __(
                 'MultilingualPress Flags requires at least PHP version 7. <br />Please ask your server administrator to update your environment to PHP version 7.',
                 'multilingualpress'
@@ -56,57 +111,48 @@ if (version_compare(PHP_VERSION, '7', '<')) {
 
             deactivate_plugins(plugin_basename(__FILE__));
         });
+        return;
     }
-    return;
-}
 
-function autoload()
-{
-    static $done;
-    if (is_bool($done)) {
-        return $done;
+    if (!is_plugin_active('multilingualpress/multilingualpress.php')) {
+        deactivateNotice(function () {
+            $message = __(
+                'You tried to activate MultilingualPress Site Flags but MultilingualPress plugin isn\'t active.',
+                'multilingualpress'
+            );
+
+            printf(
+                '<div class="notice notice-error"><span class="notice-title">%1$s</span><p>%2$s</p></div>',
+                esc_html__(
+                    'The plugin MultilingualPress Site Flags has been deactivated',
+                    'multilingualpress'
+                ),
+                wp_kses($message, ['a' => ['href' => true]])
+            );
+        });
+        return;
     }
-    if (class_exists(Flag\Flag::class)) {
-        $done = true;
 
-        return true;
+    if (!autoload()) {
+        return;
     }
-    if (is_readable(__DIR__ . '/autoload.php')) {
-        require_once __DIR__ . '/autoload.php';
-        $done = true;
 
-        return true;
-    }
-    if (is_readable(__DIR__ . '/vendor/autoload.php')) {
-        require_once __DIR__ . '/vendor/autoload.php';
-        $done = true;
-
-        return true;
-    }
-    $done = false;
-
-    return false;
-}
-
-if (!autoload()) {
-    return;
-}
-
-/**
- * Bootstraps MultilingualPress Language Flags.
- *
- * @return bool
- *
- * @wp-hook multilingualpress.add_service_providers
- */
-add_action(
-    'multilingualpress.add_service_providers',
-    function (ServiceProvidersCollection $providers) {
-        $providers
-            ->add(new Asset\ServiceProvider())
-            ->add(new Core\ServiceProvider())
-            ->add(new Flag\ServiceProvider())
-            ->add(new ServiceProvider());
-    },
-    0
-);
+    /**
+     * Bootstraps MultilingualPress Language Flags.
+     *
+     * @return bool
+     *
+     * @wp-hook multilingualpress.add_service_providers
+     */
+    add_action(
+        'multilingualpress.add_service_providers',
+        function (ServiceProvidersCollection $providers) {
+            $providers
+                ->add(new Asset\ServiceProvider())
+                ->add(new Core\ServiceProvider())
+                ->add(new Flag\ServiceProvider())
+                ->add(new ServiceProvider());
+        },
+        0
+    );
+})();
